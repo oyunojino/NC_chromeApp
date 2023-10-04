5d9629d41d15e8bf2555e0c3a5b962490bda20ab
oyunojino <dusrn2xlayjoh@gmail.com>
2023-10-04 16:38:11 +0900

    브라우저의 form 태그 기본 동작(클릭 시 url 주소로 이동) 차단

--- 1004_app/app.js
+++ 1004_app/app.js
@@ -1,13 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginSubmit(event) {
-  //   const username = loginInput.value;
-  //   console.log(username);
+const link = document.querySelector("a");
 
+function onLoginSubmit(event) {
   event.preventDefault();
   // 브라우저의 form 태그 기본 동작(submit 후 새로고침) 차단
   console.log(event);
 }
-
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function handleLinkClick(event) {
+  console.log(event);
+  // 브라우저의 form 태그 기본 동작(클릭 시 url 주소로 이동) 차단
+  alert("click");
+  // alert 창에 [ok] 클릭 시 기본 동작이 실행됨
+}
+link.addEventListener("click", handleLinkClick);
