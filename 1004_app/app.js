f6a2c49fe46e93a277c9d3e38de1ccd2ade5477b
oyunojino <dusrn2xlayjoh@gmail.com>
2023-10-04 17:05:49 +0900

    이름 입력 후 submit이 되었을 때, form 태그 숨기기

--- 1004_app/app.js
+++ 1004_app/app.js
@@ -7,6 +7,10 @@ function onLoginSubmit(event) {
   event.preventDefault();
   // 브라우저의 form 태그 기본 동작(submit 후 새로고침) 차단
   console.log(event);
+
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  console.log(username);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
 
