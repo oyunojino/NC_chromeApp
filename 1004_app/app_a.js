89bda4aa984a0c2cbe36d8c4a89a3d75d67c51ed
oyunojino <dusrn2xlayjoh@gmail.com>
2023-10-04 17:31:14 +0900

    js로 a태그 수정

--- 1004_app/app_a.js
+++ 1004_app/app_a.js
@@ -1,22 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
-
 const link = document.querySelector("a");
-
-const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   // 브라우저의 form 태그 기본 동작(submit 후 새로고침) 차단
   console.log(event);
-
-  const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  console.log(username);
-
-  greeting.innerHTML = `Hello ${username}!`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
 
