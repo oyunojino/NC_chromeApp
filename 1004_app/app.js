358eee17c2e64a37b6f701f782d33811429262d7
oyunojino <dusrn2xlayjoh@gmail.com>
2023-10-04 15:45:16 +0900

    form 태그 내에 존재할 때 유효성 검사

--- 1004_app/app.js
+++ 1004_app/app.js
@@ -8,13 +8,19 @@ function onLoginBtnClick() {
   //   console.log("hello " + loginInput.value);
 
   const username = loginInput.value;
-  if (username === "") {
-    console.log("Please write your name");
-  } else if (username.length <= 15) {
-    console.log("Your name is too long.");
-  } else {
-    console.log("hello", loginInput.value);
-  }
+  //   방법 1 ) 직접 지정
+  //   if (username === "") {
+  //     console.log("Please write your name");
+  //   } else if (username.length <= 15) {
+  //     console.log("Your name is too long.");
+  //   } else {
+  //     console.log("hello", loginInput.value);
+  //   }
+
+  //   방법 2 ) 브라우저에서 유효성 검사 지원
+  //   -> form 태그 내 해당 input 태그가 존재할 때
+  //   -> form 태그 내 button 태그, input:submit 태그, [enter]를 탭하면 자동 제출됨
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
